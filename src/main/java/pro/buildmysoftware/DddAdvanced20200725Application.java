1c7d35e190fd022f537dbf845954013564a50251
Mike Wojtyna <mike@buildmysoftware.pro>
2020-07-25 08:47:54 +0200

    Init empty project

---
+++ src/main/java/pro/buildmysoftware/DddAdvanced20200725Application.java
@@ -0,0 +1,13 @@
+package pro.buildmysoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddAdvanced20200725Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddAdvanced20200725Application.class, args);
+	}
+
+}
